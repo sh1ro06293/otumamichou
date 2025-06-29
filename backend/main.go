2c6a3179bc36e616eccbee4cb897836819ad83b6
sh1ro06293 <shiro.06293@gmail.com>
2025-06-29 16:20:52 +0000

    環境構築 DBはあとでリフレッシュトークンを保存できるように修正

---
+++ backend/main.go
@@ -0,0 +1,14 @@
+// Ginを使った例
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.String(200, "Hello, Gin!")
+	})
+	r.Run(":8000")
+}
