814bed263c48ee7d8298e9bd657e622558040e27
sh1ro06293 <shiro.06293@gmail.com>
2025-12-31 16:40:39 +0900

    feat: マイページのUIを改善し、ユーザー名に「さん」を追加、リンクをボタンに変更

--- frontend/src/pages/Mypage.tsx
+++ frontend/src/pages/Mypage.tsx
@@ -2,8 +2,8 @@
 
 import { useNavigate, useParams } from 'react-router-dom';
 import { useAuth } from '../hooks/useAuth';
-import apiClient from '../lib/axios';
 import { LogoutButton } from '../components/LogoutButton';
+import { Button } from '@chakra-ui/react';
 
 function Mypage() {
   const { user, setUser } = useAuth();
@@ -24,17 +24,17 @@ function Mypage() {
       <div>
         <h1>My Page</h1>
         <p>
-          <strong>Name:</strong> {user.name}
+          <strong>Name:</strong> {user.name} さん
         </p>
         <p>
           <strong>Email:</strong> {user.email}
         </p>
-        <LogoutButton />
-      </div>
       <div>
-        <p>
-          <a href="/otsumami">おつまみ生成</a>
-        </p>
+        <Button colorScheme="teal" size="sm" marginTop={4} marginBottom={4} background={"#666"} onClick={() => navigate('/otsumami')}>
+          おつまみ生成
+        </Button>
+      </div>
+        <LogoutButton />
       </div>
     </div>
   );
